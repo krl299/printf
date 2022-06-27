5fa08d2ef19f5f4be8f810f7cf9f61dd45480220
Carlos Moran letran <cmoran-l@c2r4s1.42malaga.com>
2022-06-27 12:55:27 +0200

    first commit

---
+++ ft_printf.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 18:52:58 by cmoran-l          #+#    #+#             */
+/*   Updated: 2022/06/27 12:38:04 by cmoran-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include "libft/libft.h"
+int	ft_printf(const char *src, ...);
+#endif
