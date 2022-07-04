97493a1be351955b6d1a1327fb9ab3d5919e0500
Carlos Moran letran <cmoran-l@c3r10s5.42malaga.com>
2022-07-04 10:35:18 +0200

    variadic function ok

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 10:56:27 by cmoran-l          #+#    #+#             */
-/*   Updated: 2022/06/27 12:53:49 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2022/07/04 10:27:52 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,35 @@
 int	ft_printf(const char *src, ...)
 {
 	va_list lstarg;
+	int		i;
+	char	dst;
+	int		conv;
 
 	va_start(lstarg, src);
+	i = 0;
+	while(src[i])
+	{
+		if (src[i] == '%')
+		{
+			conv = va_arg(lstarg, int);
+			dst = conv;
+			write(1, &dst, ft_strlen(&dst));
+			i+= 2;
+		}
+		else
+		{
+			write(1, &src[i], 1);
+			i++;
+		}
+	}
+	write(1, "\n" ,1);
 	va_end(lstarg);
 	return (0);
+}
+
+int	main()
+{
+	ft_printf("%d", 'a');
+	
+	return(0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,14 @@
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 18:52:58 by cmoran-l          #+#    #+#             */
-/*   Updated: 2022/06/27 12:38:04 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2022/06/27 13:08:07 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	FT_PRINTF_H
 # define FT_PRINTF_H
 # include <stdarg.h>
+# include <unistd.h>
 # include "libft/libft.h"
 int	ft_printf(const char *src, ...);
 #endif
